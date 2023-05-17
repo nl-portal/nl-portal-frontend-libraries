b5e7c0aa0d379409505a1d94dc27a4ac8e798e15
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-05-17 10:36:10 +0200

    updated app config interfaces with latest configuration options

--- packages/app/src/window.d.ts
+++ packages/app/src/window.d.ts
@@ -13,6 +13,11 @@ export declare global {
     OPEN_FORMS_ENTRY_ENV: string;
     OPEN_FORMS_STYLES_URL: string;
     SHOW_INHABITANT_AMOUNT: string;
+    SHOW_ADDRESS_RESEARCH: string;
     ADDRESS_RESEARCH_URL: string;
+    ACCOUNT_SHOW_NOTIFICATION_SUBSECTION: string;
+    OVERVIEW_SHOW_INTRO: string;
+    OVERVIEW_SHOW_FORMS_LINK: string;
+    OVERVIEW_SHOW_MAINTENANCE_ALERT: string;
   }
 }
