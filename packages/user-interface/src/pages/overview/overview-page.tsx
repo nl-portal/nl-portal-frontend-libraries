cc99bb440af9aec9c3999f45de2360663fe048a4
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-06-20 16:16:23 +0200

    Added personalized intro option

--- packages/user-interface/src/pages/overview/overview-page.tsx
+++ packages/user-interface/src/pages/overview/overview-page.tsx
@@ -3,14 +3,17 @@ import {Link as RouterLink} from 'react-router-dom';
 import {Paragraph} from '@gemeente-denhaag/components-react';
 import {Link} from '@gemeente-denhaag/link';
 import {Alert} from '@gemeente-denhaag/alert';
+import {Heading2, Heading3} from '@gemeente-denhaag/components-react';
 import {FormattedMessage, useIntl} from 'react-intl';
 import {FC, useContext} from 'react';
 import {LocaleContext} from '@nl-portal/nl-portal-localization';
+import {useUserInfo} from '../../utils/use-user-info';
 
 interface OverviewPageProps {
   openFormsFormId?: string;
   showFormsLink?: string;
   showIntro?: string;
+  personalizeIntro?: string;
   showAlert?: string;
   alertType?: 'error' | 'info' | 'success' | 'warning';
 }
@@ -19,11 +22,14 @@ const OverviewPage: FC<OverviewPageProps> = ({
   openFormsFormId,
   showFormsLink = 'true',
   showIntro = 'false',
+  personalizeIntro = 'false',
   showAlert = 'false',
   alertType = 'warning',
 }) => {
   const {hrefLang} = useContext(LocaleContext);
   const intl = useIntl();
+
+  const {userName, volmachtgever, isVolmachtLogin} = useUserInfo();
 
   return (
     <section>
@@ -36,12 +42,32 @@ const OverviewPage: FC<OverviewPageProps> = ({
       )}
       {showIntro === 'true' && (
         <React.Fragment>
-          <h2 className="utrecht-heading-2">
-            <FormattedMessage id="overviewpage.title" />
-          </h2>
-          <Paragraph>
-            <FormattedMessage id="overviewpage.paragraph" />
-          </Paragraph>
+          {personalizeIntro ? (
+            <React.Fragment>
+              <Heading2>
+                <FormattedMessage id="overviewpage.title" values={{userName}} />
+              </Heading2>
+              {isVolmachtLogin && (
+                <div>
+                  <Heading3>
+                    <FormattedMessage id="overviewpage.subtitle" values={{volmachtgever}} />
+                  </Heading3>
+                </div>
+              )}
+              <Paragraph>
+                <FormattedMessage id="overviewpage.paragraph" />
+              </Paragraph>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Heading2 className="utrecht-heading-2">
+                <FormattedMessage id="overviewpage.title" />
+              </Heading2>
+              <Paragraph>
+                <FormattedMessage id="overviewpage.paragraph" />
+              </Paragraph>
+            </React.Fragment>
+          )}
         </React.Fragment>
       )}
       {showFormsLink === 'true' && (
