801ddfb8be4772112f7c95e1958acebb3798f33d
FrederikJaapCampagne <frederik.campagne@denhaag.nl>
2023-09-08 18:11:58 +0200

    changed type of functions

--- packages/user-interface/src/pages/edit-account/edit-account-page.test.tsx
+++ packages/user-interface/src/pages/edit-account/edit-account-page.test.tsx
@@ -10,6 +10,65 @@ import {EditAccountPage} from './edit-account-page';
 
 const errorText = 'a valid phone number consists of 10 digits';
 
+describe('EditAccountPage', () => {
+  let inputField = () => screen.getByRole('textbox');
+  let errorTextP = () => screen.queryByText(errorText);
+  let saveButton = () => screen.getByText('Save');
+
+  beforeEach(() => {
+    render(editAccountRender());
+  });
+
+  it('should have a disabled save button if nothing is entered', () => {
+    expect(saveButton().closest('button')).toBeDisabled();
+  });
+
+  it('should give an error msg when tekst is entered as a phone number', () => {
+    expect(errorTextP()).toBeNull();
+
+    expect(inputField()).not.toBeNull();
+    
+    fireEvent.change(inputField(), {target: {value: 'hoiDitIsNietGeldig'}});
+
+    expect(errorTextP()).toBeVisible();
+    expect(saveButton().closest('button')).toBeDisabled();
+  });
+
+  it('should give an error msg when too many number are entered', () => {
+    expect(errorTextP()).toBeNull();
+
+    expect(inputField()).not.toBeNull();
+    
+    fireEvent.change(inputField(), {target: {value: '12345678910'}});
+    
+    expect(errorTextP()).not.toBeNull();
+    expect(errorTextP()?.tagName).toEqual('P');
+    expect(saveButton().closest('button')).toBeDisabled();
+  });
+
+  it('should not give an error msg when 10 numbers are entered', () => {
+    expect(errorTextP()).toBeNull();
+
+    expect(inputField()).not.toBeNull();
+    
+    fireEvent.change(inputField(), {target: {value: '0123456789'}});
+    
+    expect(errorTextP()).toBeNull();
+    expect(saveButton().closest('button')).toBeEnabled();
+  });
+
+  it('should give an error msg when a special character is entered', () => {
+    expect(errorTextP()).toBeNull();
+
+    expect(inputField()).not.toBeNull();
+
+    fireEvent.change(inputField(), {target: {value: '-012345678'}});
+
+    expect(errorTextP()).not.toBeNull();
+    expect(saveButton().closest('button')).toBeDisabled();
+  });
+});
+
 const editAccountRender = (): ReactElement => {
   const route = '/account/aanpassen?prop=telefoonnummer';
   return (
@@ -53,70 +112,3 @@ const mocks = [
     },
   },
 ];
-
-describe('EditAccountPage', () => {
-  let inputField = () => screen.queryByRole('textbox');
-  let errorTextP = () => screen.queryByText(errorText);
-  let saveButton = () => screen.queryByText('Save');
-
-  beforeEach(() => {
-    render(editAccountRender());
-  });
-
-  it('should have a disabled save button if nothing is entered', () => {
-    expect(saveButton()?.closest('button')).toBeDisabled();
-  });
-
-  it('should give an error msg when tekst is entered as a phone number', () => {
-    expect(errorTextP()).toBeNull();
-
-    const textbox = inputField();
-    expect(textbox).not.toBeNull();
-    if (textbox) {
-      fireEvent.change(textbox, {target: {value: 'hoiDitIsNietGeldig'}});
-    }
-
-    expect(errorTextP()).toBeVisible();
-    expect(saveButton()?.closest('button')).toBeDisabled();
-  });
-
-  it('should give an error msg when too many number are entered', () => {
-    expect(errorTextP()).toBeNull();
-
-    const textbox = inputField();
-    expect(textbox).not.toBeNull();
-    if (textbox) {
-      fireEvent.change(textbox, {target: {value: '12345678910'}});
-    }
-
-    expect(errorTextP()).not.toBeNull();
-    expect(errorTextP()?.tagName).toEqual('P');
-    expect(saveButton()?.closest('button')).toBeDisabled();
-  });
-
-  it('should not give an error msg when 10 numbers are entered', () => {
-    expect(errorTextP()).toBeNull();
-
-    const textbox = inputField();
-    expect(textbox).not.toBeNull();
-    if (textbox) {
-      fireEvent.change(textbox, {target: {value: '0123456789'}});
-    }
-
-    expect(errorTextP()).toBeNull();
-    expect(saveButton()?.closest('button')).toBeEnabled();
-  });
-
-  it('should give an error msg when a special character is entered', () => {
-    expect(errorTextP()).toBeNull();
-
-    const textbox = inputField();
-    expect(textbox).not.toBeNull();
-    if (textbox) {
-      fireEvent.change(textbox, {target: {value: '-012345678'}});
-    }
-
-    expect(errorTextP()).not.toBeNull();
-    expect(saveButton()?.closest('button')).toBeDisabled();
-  });
-});
