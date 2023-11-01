a6eac80cb9c2761167f8fb948591a2e8bc40b953
FrederikJaapCampagne <frederik.campagne@denhaag.nl>
2023-11-01 17:17:24 +0100

    separate component and test

--- packages/user-interface/src/pages/edit-account/edit-account-page.test.tsx
+++ packages/user-interface/src/pages/edit-account/edit-account-page.test.tsx
@@ -1,56 +1,5 @@
-import React, {ReactElement} from 'react';
 import {fireEvent, render, screen} from '@testing-library/react';
-import {MemoryRouter} from 'react-router-dom';
-import {MockedProvider} from '@apollo/client/testing';
-import {gql} from '@apollo/client';
-import {LocalizationProvider} from '@nl-portal/nl-portal-localization';
-import {UserInformationProvider} from '../../providers';
-import {EditAccountPage} from './edit-account-page';
-
-const MockEditAccountPage = (): ReactElement => {
-  const route = '/account/aanpassen?prop=telefoonnummer';
-
-  const EXAMPLE_QUERY = gql`
-    mutation SubmitTask($id: UUID!, $submission: JSON!) {
-      submitTask(id: $id, submission: $submission) {
-        id
-        objectId
-        formId
-        title
-        status
-        date
-      }
-    }
-  `;
-
-  const mocks = [
-    {
-      request: {
-        query: EXAMPLE_QUERY,
-        variables: {
-          name: 'Test',
-        },
-      },
-      result: {
-        data: {
-          task: {id: '1', name: 'Informatie aanvraag', zaakId: '23jfdi45-2345-2345-jf45k323oi12'},
-        },
-      },
-    },
-  ];
-
-  return (
-    <LocalizationProvider>
-      <UserInformationProvider>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <MemoryRouter initialEntries={[route]}>
-            <EditAccountPage />
-          </MemoryRouter>
-        </MockedProvider>
-      </UserInformationProvider>
-    </LocalizationProvider>
-  );
-};
+import { MockEditAccountPage } from './mock-edit-account-page';
 
 describe('EditAccountPage', () => {
   const errorText = 'a valid phone number consists of 10 digits';
