93b0f580ef852e0edddb5abc7ceee491d0c5b030
FrederikJaapCampagne <frederik.campagne@denhaag.nl>
2023-12-11 11:47:35 +0100

    added checks for account page

--- packages/user-interface/src/pages/account/account-page.test.tsx
+++ packages/user-interface/src/pages/account/account-page.test.tsx
@@ -24,5 +24,12 @@ describe('AccountPage', () => {
       expect(screen.getByText('Sierra')).toBeInTheDocument();
     });
     expect(screen.getByText('Nederlandse, Portugees')).toBeVisible();
+    expect(screen.getByText('Kooyman')).toBeVisible();
+    expect(screen.getByText('vrouw')).toBeVisible();
+    expect(screen.getByText('999991954')).toBeVisible();
+    expect(screen.getByText('3 maart 2003')).toBeVisible();
+    expect(screen.getByText('Nederland')).toBeVisible();
+    expect(screen.getByText('Leyweg 61e')).toBeVisible();
+    expect(screen.getByText("2545CC 's-Gravenhage")).toBeVisible();
   });
 });
--- packages/user-interface/src/testUtils/custom-messages/custom-messages.ts
+++ packages/user-interface/src/testUtils/custom-messages/custom-messages.ts
@@ -2,6 +2,8 @@ import {Messages} from '@nl-portal/nl-portal-localization';
 import {NL_NL_MESSAGES} from './nl-nl';
 import {EN_GB_MESSAGES} from './en-gb';
 
+//Placed in this folder because it cannot be exported, but in the future this will probably change
+
 export const CUSTOM_MESSAGES: Messages = {
   ...NL_NL_MESSAGES,
   ...EN_GB_MESSAGES,
