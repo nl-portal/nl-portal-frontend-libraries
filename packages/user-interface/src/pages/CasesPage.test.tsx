347832d1ce5b7c481770da5f0aebf620daaa9a5f
Erwin <63862196+lampie1973@users.noreply.github.com>
2024-02-05 10:56:28 +0100

    formatted code

--- packages/user-interface/src/pages/CasesPage.test.tsx
+++ packages/user-interface/src/pages/CasesPage.test.tsx
@@ -6,26 +6,32 @@ describe("The CasesPage", () => {
   it("should render several cases", async () => {
     render(MockCasesPage());
     await waitFor(() => {
-      expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0]).toBeVisible();
+      expect(
+        screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0],
+      ).toBeVisible();
     });
-    screen.debug();
     expect(screen.getAllByText("case.B0756.title")[0]).not.toBeVisible();
-    expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title").length).toEqual(2);
+    expect(
+      screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title").length,
+    ).toEqual(2);
     expect(screen.getAllByText("1-2-2024")[0]).toBeVisible();
   });
 
-  it("should allow me to go to the closes cases", async () => {
+  it("should allow me to go to the overview of closes cases", async () => {
     render(MockCasesPage());
     await waitFor(() => {
-      expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0]).toBeVisible();
+      expect(
+        screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0],
+      ).toBeVisible();
     });
-    const tabAfgerondeZaken = screen.getByRole("tab", {name: "Closed cases"});
+    
+    const tabAfgerondeZaken = screen.getByRole("tab", { name: "Closed cases" });
     fireEvent.click(tabAfgerondeZaken);
-    screen.debug();
     expect(screen.getAllByText("case.B0756.title")[0]).toBeVisible();
-    expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0]).not.toBeVisible();
+    expect(
+      screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0],
+    ).not.toBeVisible();
     expect(screen.getAllByText("case.B0756.title").length).toEqual(2);
     expect(screen.getAllByText("2-2-2024")[0]).toBeVisible();
   });
-
 });
