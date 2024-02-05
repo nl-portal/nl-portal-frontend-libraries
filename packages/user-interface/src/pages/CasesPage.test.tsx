e58bdbaeed25013a3fe781691d9eb9d64b453919
FrederikJaapCampagne <frederik.campagne@denhaag.nl>
2024-02-05 21:42:22 +0100

    added a few checks and refactored a bit

--- packages/user-interface/src/pages/CasesPage.test.tsx
+++ packages/user-interface/src/pages/CasesPage.test.tsx
@@ -3,35 +3,49 @@ import { MockCasesPage } from "./CasesPage.mock";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 describe("The CasesPage", () => {
-  it("should render several cases", async () => {
+  let tabLopendeZaken = () =>
+    screen.getByRole("tab", { name: "Current cases" });
+  let tabAfgerondeZaken = () =>
+    screen.getByRole("tab", { name: "Closed cases" });
+  let getAllBezwaarCases = () => screen.getAllByText("case.B0756.title");
+  let getAllOpasCases = () =>
+    screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title");
+
+  it("should render several active cases", async () => {
     render(MockCasesPage());
+
     await waitFor(() => {
-      expect(
-        screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0],
-      ).toBeVisible();
+      expect(getAllOpasCases()[0]).toBeVisible();
     });
-    expect(screen.getAllByText("case.B0756.title")[0]).not.toBeVisible();
-    expect(
-      screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title").length,
-    ).toEqual(2);
-    expect(screen.getAllByText("1-2-2024")[0]).toBeVisible();
+
+    expect(tabLopendeZaken().getAttribute("aria-selected")).toBe("true");
+    expect(tabAfgerondeZaken().getAttribute("aria-selected")).toBe("false");
+
+    expect(getAllBezwaarCases()[0]).not.toBeVisible();
+
+    expect(getAllOpasCases().length).toEqual(2);
+
+    expect(screen.getByText("2/3/2024")).toBeVisible();
   });
 
-  it("should allow me to go to the overview of closes cases", async () => {
+  it("should allow me to go to the overview of closed cases", async () => {
     render(MockCasesPage());
+
     await waitFor(() => {
-      expect(
-        screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0],
-      ).toBeVisible();
+      expect(getAllOpasCases()[0]).toBeVisible();
     });
-    
-    const tabAfgerondeZaken = screen.getByRole("tab", { name: "Closed cases" });
-    fireEvent.click(tabAfgerondeZaken);
-    expect(screen.getAllByText("case.B0756.title")[0]).toBeVisible();
-    expect(
-      screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0],
-    ).not.toBeVisible();
-    expect(screen.getAllByText("case.B0756.title").length).toEqual(2);
-    expect(screen.getAllByText("2-2-2024")[0]).toBeVisible();
+
+    fireEvent.click(tabAfgerondeZaken());
+
+    expect(tabLopendeZaken().getAttribute("aria-selected")).toBe("false");
+    expect(tabAfgerondeZaken().getAttribute("aria-selected")).toBe("true");
+
+    expect(getAllBezwaarCases()[0]).toBeVisible();
+
+    expect(getAllOpasCases()[0]).not.toBeVisible();
+
+    expect(getAllBezwaarCases().length).toEqual(2);
+
+    expect(screen.getAllByText("2/1/2024")[0]).toBeVisible();
   });
 });
