04ff2fe1d86e1c0055f6582a1186ecb1243b282a
Erwin <63862196+lampie1973@users.noreply.github.com>
2024-02-05 10:36:41 +0100

    fixed test

--- packages/user-interface/src/pages/CasesPage.test.tsx
+++ packages/user-interface/src/pages/CasesPage.test.tsx
@@ -1,15 +1,31 @@
 import { describe, it, expect } from "vitest";
 import { MockCasesPage } from "./CasesPage.mock";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
-describe("CasesPage", () => {
+describe("The CasesPage", () => {
   it("should render several cases", async () => {
     render(MockCasesPage());
     await waitFor(() => {
       expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0]).toBeVisible();
     });
-    expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title").length).toEqual(67);
-    //@TODO fix language settings
+    screen.debug();
+    expect(screen.getAllByText("case.B0756.title")[0]).not.toBeVisible();
+    expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title").length).toEqual(2);
     expect(screen.getAllByText("1-2-2024")[0]).toBeVisible();
   });
+
+  it("should allow me to go to the closes cases", async () => {
+    render(MockCasesPage());
+    await waitFor(() => {
+      expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0]).toBeVisible();
+    });
+    const tabAfgerondeZaken = screen.getByRole("tab", {name: "Closed cases"});
+    fireEvent.click(tabAfgerondeZaken);
+    screen.debug();
+    expect(screen.getAllByText("case.B0756.title")[0]).toBeVisible();
+    expect(screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title")[0]).not.toBeVisible();
+    expect(screen.getAllByText("case.B0756.title").length).toEqual(2);
+    expect(screen.getAllByText("2-2-2024")[0]).toBeVisible();
+  });
+
 });
