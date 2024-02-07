7451991090a890dd20e12f71d043cbfc243e07ae
Erwin <63862196+lampie1973@users.noreply.github.com>
2024-02-07 06:25:37 +0100

    added tabtests

--- packages/user-interface/src/pages/CasesPage.test.tsx
+++ packages/user-interface/src/pages/CasesPage.test.tsx
@@ -3,20 +3,20 @@ import { MockCasesPage } from "./CasesPage.mock";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 describe("The CasesPage", () => {
-  let tabLopendeZaken = () =>
+  const tabLopendeZaken = () =>
     screen.getByRole("tab", { name: "Current cases" });
-  let tabAfgerondeZaken = () =>
+  const tabAfgerondeZaken = () =>
     screen.getByRole("tab", { name: "Closed cases" });
-  let getAllBezwaarCases = () => screen.getAllByText("case.B0756.title");
-  let getAllOpasCases = () =>
-    screen.getAllByText("case.Aanvraag-stadspas-behandelen-v6.title");
 
   it("should render several active cases", async () => {
     render(MockCasesPage());
-
     await waitFor(() => {
       expect(screen.getByText("case.OPENZAAK1.title")).toBeVisible();
     });
+
+    expect(tabLopendeZaken().getAttribute("aria-selected")).toBe("true");
+    expect(tabAfgerondeZaken().getAttribute("aria-selected")).toBe("false");
+
     expect(screen.getByText("case.GESLOTENZAAK1.title")).not.toBeVisible();
     expect(screen.getByText("case.GESLOTENZAAK2.title")).not.toBeVisible();
     expect(screen.getByText("case.OPENZAAK1.title")).toBeVisible();
@@ -25,11 +25,14 @@ describe("The CasesPage", () => {
 
   it("should allow me to go to the overview of closed cases", async () => {
     render(MockCasesPage());
-    const tabAfgerondeZaken = screen.getByRole("tab", { name: "Closed cases" });
-    fireEvent.click(tabAfgerondeZaken);
+    fireEvent.click(tabAfgerondeZaken());
     await waitFor(() => {
       expect(screen.getByText("case.GESLOTENZAAK1.title")).toBeVisible();
     });
+
+    expect(tabLopendeZaken().getAttribute("aria-selected")).toBe("false");
+    expect(tabAfgerondeZaken().getAttribute("aria-selected")).toBe("true");
+
     expect(screen.getByText("case.GESLOTENZAAK1.title")).toBeVisible();
     expect(screen.getByText("case.GESLOTENZAAK2.title")).toBeVisible();
     expect(screen.getByText("case.OPENZAAK1.title")).not.toBeVisible();
