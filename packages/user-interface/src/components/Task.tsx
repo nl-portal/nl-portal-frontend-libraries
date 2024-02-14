af1002acdd9f1896954431b0237e5e7bb299301c
Borai van Uden <info@borai.nl>
2024-02-14 09:44:31 +0100

    feat: replace Task with Action component

--- packages/user-interface/src/components/Task.tsx
+++ packages/user-interface/src/components/Task.tsx
@@ -1,8 +1,19 @@
 import { GetTakenQuery } from "@nl-portal/nl-portal-api";
-import { SubjectCard } from "@gemeente-denhaag/card";
 import PortalLink from "./PortalLink";
-import styles from "./Task.module.scss";
 import { getTaskUrl } from "../utils/get-task-url";
+import { Action } from "@gemeente-denhaag/action";
+
+const labels = {
+  today: "vandaag",
+  yesterday: "gisteren",
+  before: "vóór",
+  approachingDeadline: (daysDifference: number) => {
+    if (daysDifference === 1) {
+      return `nog ${daysDifference} dag`;
+    }
+    return `nog ${daysDifference} dagen`;
+  },
+};
 
 interface Props {
   task: GetTakenQuery["getTaken"]["content"][0];
@@ -10,18 +21,16 @@ interface Props {
 
 const Task = ({ task }: Props) => {
   return (
-    <div className={styles.task}>
-      <SubjectCard
-        title={task.title}
-        date={new Date(task.date)}
-        Link={PortalLink}
-        href={getTaskUrl(
-          task.formulier.formuliertype,
-          task.formulier.value,
-          task.id,
-        )}
-      />
-    </div>
+    <Action
+      labels={labels}
+      link={getTaskUrl(
+        task.formulier.formuliertype,
+        task.formulier.value,
+        task.id,
+      )}
+    >
+      {task.title}
+    </Action>
   );
 };
 
