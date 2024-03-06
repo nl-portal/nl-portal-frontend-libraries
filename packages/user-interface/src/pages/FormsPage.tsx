32e31f1cb8eec3105ecec756461f1d5b5d98384b
mi <maykel.ibrahim@ritense.com>
2024-03-06 11:36:18 +0100

    Added list styling

--- packages/user-interface/src/pages/FormsPage.tsx
+++ packages/user-interface/src/pages/FormsPage.tsx
@@ -7,18 +7,33 @@ import { RouterOutletContext } from "../contexts/RouterOutletContext.tsx";
 import { Link } from "react-router-dom";
 
 
-const FormsPage = ({forms}) => {
+const FormsPage = ({ forms }) => {
   const { paths } = useOutletContext<RouterOutletContext>();
   return (
     <PageGrid>
       <PageHeader title={<FormattedMessage id="pageTitles.forms" />} />
       {forms && forms.length > 0 && (
-        <ul>
+        <ul class="utrecht-link-list utrecht-link-list--html-ul denhaag-link-group__list">
           {forms.map((form) => {
             return (
-              <li key={`${form}`}>
+              <li class="denhaag-link-group__list-item" key={`${form}`}>
                 <Link to={paths.form(form)}>
-                  <FormattedMessage id={`forms.${form}`} />
+                  <span className="denhaag-link denhaag-link--with-icon denhaag-link--with-icon-start">
+                    <svg  aria-hidden="true"
+                          className="denhaag-icon"
+                          focusable="false"
+                          height="1em"
+                          viewBox="0 0 24 24"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M12.293 5.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L16.586 13H5a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        fill="currentColor"></path>
+                    </svg>
+                  </span>
+                  <span className="denhaag-link__label">
+                    <FormattedMessage id={`forms.${form}`} />
+                  </span>
                 </Link>
               </li>
             );
@@ -31,7 +46,7 @@ const FormsPage = ({forms}) => {
         </Paragraph>
       )}
     </PageGrid>
-);
+  );
 };
 
 export default FormsPage;
