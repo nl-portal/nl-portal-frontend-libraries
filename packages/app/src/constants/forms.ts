976736bd35341fc21a560b8dd856c0f554b005bc
mi <maykel.ibrahim@ritense.com>
2024-03-07 16:07:22 +0100

    Fixed type

--- packages/app/src/constants/forms.ts
+++ packages/app/src/constants/forms.ts
@@ -1,4 +1,6 @@
-export const forms: [String] = [
+import { Forms } from "@nl-portal/nl-portal-user-interface/src/interfaces/forms.ts";
+
+export const forms: Forms = [
   'aanvraag-1',
   'aanvraag-2'
 ];
