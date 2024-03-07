db3982392e9b9baad7f058dcb99b60c25c83d1d5
mi <maykel.ibrahim@ritense.com>
2024-03-07 16:31:27 +0100

    Fixed undefined forms situation

--- packages/user-interface/src/pages/FormPage.tsx
+++ packages/user-interface/src/pages/FormPage.tsx
@@ -5,13 +5,14 @@ import { useParams } from "react-router-dom";
 import { Forms } from "../interfaces/forms";
 
 const FormPage = ({ forms }: Forms) => {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
+  const safeId = id || "";
   return (
     <PageGrid>
       <PageHeader title={
-        (forms && forms.length > 0 && forms.includes(id)) ? (
-          <span data-testid={`form-item-${id}`}>
-            <FormattedMessage id={`forms.${id}`} />
+        (forms && forms.length > 0 && forms.includes(safeId)) ? (
+          <span data-testid={`form-item-${safeId}`}>
+            <FormattedMessage id={`forms.${safeId}`} />
           </span>
         ) : (
           <span data-testid={`form-not-found`}>
