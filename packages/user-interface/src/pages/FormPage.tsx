5f0e2fb4e1e32af232857f54d8a41f98c26a9255
mi <maykel.ibrahim@ritense.com>
2024-03-07 16:26:27 +0100

    Fixed undefined forms situation

--- packages/user-interface/src/pages/FormPage.tsx
+++ packages/user-interface/src/pages/FormPage.tsx
@@ -2,18 +2,18 @@ import PageGrid from "../components/PageGrid";
 import PageHeader from "../components/PageHeader";
 import { FormattedMessage } from "react-intl";
 import { useParams } from "react-router-dom";
-import { Forms } from "../interfaces/forms.ts";
+import { Forms } from "../interfaces/forms";
 
 const FormPage = ({ forms }: Forms) => {
   const { id } = useParams();
   return (
     <PageGrid>
       <PageHeader title={
-        forms && forms.length > 0 && forms.includes(id) && (
+        (forms && forms.length > 0 && forms.includes(id)) ? (
           <span data-testid={`form-item-${id}`}>
             <FormattedMessage id={`forms.${id}`} />
           </span>
-        ) || forms && (!forms.length || (forms.length > 0 && !forms.includes(id))) && (
+        ) : (
           <span data-testid={`form-not-found`}>
             <FormattedMessage id="form.formNotFound" />
           </span>
--- packages/user-interface/src/pages/FormsPage.tsx
+++ packages/user-interface/src/pages/FormsPage.tsx
@@ -3,7 +3,7 @@ import PageHeader from "../components/PageHeader";
 import { FormattedMessage } from "react-intl";
 import { Link } from "@gemeente-denhaag/link";
 import { ArrowRightIcon } from "@gemeente-denhaag/icons";
-import { Forms } from "../interfaces/forms.ts";
+import { Forms } from "../interfaces/forms";
 
 const FormsPage = ({ forms }: Forms) => {
   return (
@@ -18,20 +18,20 @@ const FormsPage = ({ forms }: Forms) => {
       {forms && forms.length > 0 && (
         <ul data-testid={"list-forms"}
             className="utrecht-link-list utrecht-link-list--html-ul denhaag-link-group__list">
-            {forms.map((form) => {
-              return (
-                <li className="denhaag-link-group__list-item" key={`${form}`}>
-                  <Link href={`/formulieren/formulier/${form}`}
-                        icon={<ArrowRightIcon />}
-                        iconAlign="start">
+          {forms.map((form) => {
+            return (
+              <li className="denhaag-link-group__list-item" key={`${form}`}>
+                <Link href={`/formulieren/formulier/${form}`}
+                      icon={<ArrowRightIcon />}
+                      iconAlign="start">
                     <span data-testid={`forms-item-${form}`}>
                       <FormattedMessage id={`forms.${form}`} />
                     </span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       )}
     </PageGrid>
   );
