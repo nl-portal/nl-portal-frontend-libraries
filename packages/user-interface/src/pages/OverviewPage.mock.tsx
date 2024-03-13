90544248dcebb6cb31446e5b5e80f13cdbec5273
Lampie1973 <63862196+lampie1973@users.noreply.github.com>
2024-03-13 15:24:08 +0100

    fix built error

--- packages/user-interface/src/pages/OverviewPage.mock.tsx
+++ packages/user-interface/src/pages/OverviewPage.mock.tsx
@@ -1,11 +1,31 @@
 import OverviewPage from "./OverviewPage";
-import TestProvider from "../providers/TestProvider";
+import TestProvider, { testPaths as paths } from "../providers/TestProvider";
 import { mockRequest } from "./CasesPageRequests.mock";
+import CasesPage from "./CasesPage";
 
-const route = "/zaken/zaak?id=82cb13cf-d2f9-4e3e-ac07-751373035ecb";
+//const route = "/zaken/zaak?id=82cb13cf-d2f9-4e3e-ac07-751373035ecb";
+
+const route = [
+  {
+    path: paths.overview,
+    element: <></>,
+  },
+  {
+    path: paths.cases,
+    children: [
+      {
+        index: true,
+        element: <CasesPage />,
+      },
+    ],
+  },
+];
 
 export const MockOverviewPage = () => (
-  <TestProvider mocks={mockRequest} route={route}>
-    <OverviewPage/>
-  </TestProvider>
+  <TestProvider
+  mocks={mockRequest}
+  routes={route}
+  initialIndex={1}
+  initialEntries={[paths.overview, paths.cases]}
+></TestProvider>
 );
