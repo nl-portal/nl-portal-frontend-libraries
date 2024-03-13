1277de394f9a004cfed256565dc0a44412427914
Lampie1973 <63862196+lampie1973@users.noreply.github.com>
2024-03-13 15:33:01 +0100

    removed comment

--- packages/user-interface/src/pages/OverviewPage.mock.tsx
+++ packages/user-interface/src/pages/OverviewPage.mock.tsx
@@ -2,8 +2,6 @@ import TestProvider, { testPaths as paths } from "../providers/TestProvider";
 import { mockRequest } from "./CasesPageRequests.mock";
 import CasesPage from "./CasesPage";
 
-//const route = "/zaken/zaak?id=82cb13cf-d2f9-4e3e-ac07-751373035ecb";
-
 const route = [
   {
     path: paths.overview,
