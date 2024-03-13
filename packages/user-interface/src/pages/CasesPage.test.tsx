5bec02a75904a699da9fdfb8dfd6a760e97b2407
Lampie1973 <63862196+lampie1973@users.noreply.github.com>
2024-03-13 15:01:27 +0100

    fixed failing tests

--- packages/user-interface/src/pages/CasesPage.test.tsx
+++ packages/user-interface/src/pages/CasesPage.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { MockCasesPage } from "./CasesPage.mock";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { testPaths as paths } from "../providers/TestProvider";
 
 describe("The CasesPage", () => {
 
@@ -34,16 +35,14 @@ describe("The CasesPage", () => {
     let screenDate = new Date(Date.parse(openZaak1Date));
     expect(screen.getByText(screenDate.toLocaleDateString())).toBeVisible();
     expect(screen.getByRole("link", {name: "case.OPENZAAK1.title"})).toHaveAttribute(
-      "href",
-      "/zaken/zaak?id=6f268986-17c2-4045-9340-94101bfad3ca",
+      "href", paths.case("6f268986-17c2-4045-9340-94101bfad3ca"),
     );
 
     expect(openZaak2()).toBeVisible();
     screenDate = new Date(Date.parse(openZaak2Date));
     expect(screen.getByText(screenDate.toLocaleDateString())).toBeVisible();
     expect(screen.getByRole("link", {name: "case.OPENZAAK2.title"})).toHaveAttribute(
-      "href",
-      "/zaken/zaak?id=009e2451-44b3-4969-91e3-205d8b261fe1",
+      "href", paths.case("009e2451-44b3-4969-91e3-205d8b261fe1"),
     );
 
     expect(geslotenZaak1()).not.toBeVisible();
@@ -64,15 +63,13 @@ describe("The CasesPage", () => {
     let screenDate = new Date(Date.parse(geslotenZaak1Date));
     expect(screen.getByText(screenDate.toLocaleDateString())).toBeVisible();
     expect(screen.getByRole("link", {name: "case.GESLOTENZAAK1.title"})).toHaveAttribute(
-      "href",
-      "/zaken/zaak?id=0c6c5300-fd08-4fae-977d-c85a2c7535e8",
+      "href", paths.case("0c6c5300-fd08-4fae-977d-c85a2c7535e8"),
     );
     expect(geslotenZaak2()).toBeVisible();
     screenDate = new Date(Date.parse(geslotenZaak2Date));
     expect(screen.getByText(screenDate.toLocaleDateString())).toBeVisible();
     expect(screen.getByRole("link", {name: "case.GESLOTENZAAK2.title"})).toHaveAttribute(
-      "href",
-      "/zaken/zaak?id=e7c34f50-1d2e-4269-8eef-18da509358f4",
+      "href", paths.case("e7c34f50-1d2e-4269-8eef-18da509358f4"),
     );
 
     expect(openZaak1()).not.toBeVisible();
--- packages/user-interface/src/pages/OverviewPage.test.tsx
+++ packages/user-interface/src/pages/OverviewPage.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import { MockCasesPage } from "./CasesPage.mock";
+import { testPaths as paths } from "../providers/TestProvider";
 
 describe("OverviewPage", () => {
   const openZaak1 = () => screen.getByText("case.OPENZAAK1.title");
@@ -22,8 +23,7 @@ describe("OverviewPage", () => {
     expect(
       screen.getByRole("link", { name: "case.OPENZAAK1.title" }),
     ).toHaveAttribute(
-      "href",
-      "/zaken/zaak?id=6f268986-17c2-4045-9340-94101bfad3ca",
+      "href", paths.case("6f268986-17c2-4045-9340-94101bfad3ca"),
     );
 
     expect(openZaak2()).toBeVisible();
@@ -32,8 +32,7 @@ describe("OverviewPage", () => {
     expect(
       screen.getByRole("link", { name: "case.OPENZAAK2.title" }),
     ).toHaveAttribute(
-      "href",
-      "/zaken/zaak?id=009e2451-44b3-4969-91e3-205d8b261fe1",
+      "href", paths.case("009e2451-44b3-4969-91e3-205d8b261fe1"),
     );
 
     expect(geslotenZaak1()).not.toBeVisible();
