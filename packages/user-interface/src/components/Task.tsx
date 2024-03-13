72b202fc71e49d856707a6003c3d54144a66600d
Borai van Uden <info@borai.nl>
2024-03-13 12:26:55 +0100

    feat: add tasks and cases to details page

--- packages/user-interface/src/components/Task.tsx
+++ packages/user-interface/src/components/Task.tsx
@@ -1,8 +1,10 @@
 import { GetTakenQuery } from "@nl-portal/nl-portal-api";
 import PortalLink from "./PortalLink";
 import { getTaskUrl } from "../utils/get-task-url";
-import { Action } from "@gemeente-denhaag/action";
+import { ActionMulti, ActionSingle } from "@gemeente-denhaag/action";
 import useActionLabels from "../hooks/useActionLabels";
+import { ButtonLink } from "@gemeente-denhaag/button-link";
+import { ChevronRightIcon } from "@gemeente-denhaag/icons";
 
 interface Props {
   task: GetTakenQuery["getTaken"]["content"][0];
@@ -10,19 +12,36 @@ interface Props {
 
 const Task = ({ task }: Props) => {
   const labels = useActionLabels();
+  const link = getTaskUrl(
+    task.formulier.formuliertype,
+    task.formulier.value,
+    task.id,
+  );
+
+  if (task.formulier.formuliertype === "externalurl")
+    return (
+      <ActionMulti
+        labels={labels}
+        dateTime={task.verloopdatum}
+        actions={
+          <ButtonLink href={task} target="_blank">
+            <ChevronRightIcon />
+          </ButtonLink>
+        }
+      >
+        {task.title}
+      </ActionMulti>
+    );
 
   return (
-    <Action
+    <ActionSingle
       labels={labels}
-      link={getTaskUrl(
-        task.formulier.formuliertype,
-        task.formulier.value,
-        task.id,
-      )}
+      dateTime={task.verloopdatum}
+      link={link}
       Link={PortalLink}
     >
       {task.title}
-    </Action>
+    </ActionSingle>
   );
 };
 
--- packages/user-interface/src/pages/ThemeDetailsPage.tsx
+++ packages/user-interface/src/pages/ThemeDetailsPage.tsx
@@ -1,6 +1,12 @@
 import { useIntl } from "react-intl";
 import PageGrid from "../components/PageGrid";
 import PageHeader from "../components/PageHeader";
+import BackLink from "../components/BackLink";
+import { useOutletContext } from "react-router-dom";
+import { RouterOutletContext } from "../contexts/RouterOutletContext";
+import { useGetTakenQuery, useGetZakenQuery } from "@nl-portal/nl-portal-api";
+import TasksList from "../components/TasksList";
+import CasesList from "../components/CasesList";
 
 interface Props {
   type: string;
@@ -8,10 +14,38 @@ interface Props {
 
 const ThemeDetailsPage = ({ type }: Props) => {
   const intl = useIntl();
+  const { paths } = useOutletContext<RouterOutletContext>();
+  const {
+    data: taskData,
+    loading: taskLoading,
+    error: taskError,
+  } = useGetTakenQuery();
+  const {
+    data: caseData,
+    loading: caseLoading,
+    error: caseError,
+  } = useGetZakenQuery();
+  const loading = taskLoading || caseLoading;
 
   return (
     <PageGrid>
-      <PageHeader title={intl.formatMessage({ id: `pageTitles.${type}` })} />
+      <div>
+        <BackLink routePath={paths.themeOverview(type)} />
+        <PageHeader title={intl.formatMessage({ id: `pageTitles.${type}` })} />
+      </div>
+      <TasksList
+        loading={loading}
+        error={Boolean(taskError)}
+        tasks={taskData?.getTaken.content}
+        showTitle={false}
+      />
+      <CasesList
+        loading={loading}
+        error={Boolean(caseError)}
+        cases={caseData?.getZaken.filter(
+          (c) => !c.status?.statustype.isEindstatus,
+        )}
+      />
     </PageGrid>
   );
 };
