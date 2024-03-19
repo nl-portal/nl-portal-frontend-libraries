2c1b41d9fce20e74e2926ef6236fb1c6041206f8
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-03-19 17:19:56 +0100

    fix: removed unused import

--- packages/user-interface/src/tests/components/PageMetaData.test.tsx
+++ packages/user-interface/src/tests/components/PageMetaData.test.tsx
@@ -1,7 +1,6 @@
 import { describe, it, expect, beforeEach, afterEach } from "vitest";
 import { render } from "@testing-library/react";
 import { MockWrapper } from "@nl-portal/nl-portal-localization";
-import { unmountComponentAtNode } from "react-dom";
 import Page from "../../components/PageMetaData";
 import { NavigationItem } from "../../interfaces/navigation-item";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
