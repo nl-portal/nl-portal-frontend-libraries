cabd69d0860eca20b46907c71d897b05dbf0f7af
Borai van Uden <info@borai.nl>
2024-07-01 13:04:52 +0200

    fix: tests

--- packages/user-interface/src/tests/mock/pages/EditAccountPage.mock.tsx
+++ packages/user-interface/src/tests/mock/pages/EditAccountPage.mock.tsx
@@ -1,8 +1,8 @@
-import { gql } from "@apollo/client";
 import EditAccountPage from "../../../pages/EditAccountPage";
 import TestProvider, {
   testPaths as paths,
 } from "../../../providers/TestProvider";
+import { getBurgerProfiel } from "../data/AllRequests.mock";
 
 export const MockEditAccountPage = () => {
   const routes = [
@@ -17,42 +17,9 @@ export const MockEditAccountPage = () => {
     },
   ];
 
-  const EXAMPLE_QUERY = gql`
-    mutation SubmitTask($id: UUID!, $submission: JSON!) {
-      submitTask(id: $id, submission: $submission) {
-        id
-        objectId
-        formId
-        title
-        status
-        date
-      }
-    }
-  `;
-
-  const mocks = [
-    {
-      request: {
-        query: EXAMPLE_QUERY,
-        variables: {
-          name: "Test",
-        },
-      },
-      result: {
-        data: {
-          task: {
-            id: "1",
-            name: "Informatie aanvraag",
-            zaakId: "23jfdi45-2345-2345-jf45k323oi12",
-          },
-        },
-      },
-    },
-  ];
-
   return (
     <TestProvider
-      mocks={mocks}
+      mocks={[getBurgerProfiel]}
       routes={routes}
       initialIndex={1}
       initialEntries={[
