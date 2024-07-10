1c1a52a64f591ce6f224d2a0047ffdc4cfada5c8
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-10 14:33:35 +0200

    fix: changed duplicate name

--- packages/api/src/queries/get-formtaak-by-id-v2.ts
+++ packages/api/src/queries/get-formtaak-by-id-v2.ts
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_GET_TAAK_BY_ID_V2 = gql`
+export const QUERY_GET_FORMTAAK_BY_ID_V2 = gql`
   query GetFormTaakByIdV2($id: UUID!) {
     getTaakByIdV2(id: $id) {
       id
