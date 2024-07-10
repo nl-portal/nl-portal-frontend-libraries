17b2ac4f951cdf0d72ff1a10e9a36d20ae0bd2ab
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-10 12:56:40 +0200

    feat: removed unused formulier fields

--- packages/api/src/queries/get-taak-by-id-v2.ts
+++ packages/api/src/queries/get-taak-by-id-v2.ts
@@ -1,8 +1,6 @@
 import { gql } from "@apollo/client";
-import { FORMULIER_FIELDS } from "../fragments/formulier";
 
 export const QUERY_GET_TAAK_BY_ID_V2 = gql`
-  ${FORMULIER_FIELDS}
   query GetTaakByIdV2($id: UUID!) {
     getTaakByIdV2(id: $id) {
       id
