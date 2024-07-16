3999c81ad4bbfbfe0ee319e893d54b924c02973b
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-16 17:31:40 +0200

    refactor: task component with taak v2 soorten

--- packages/user-interface/src/components/Task.tsx
+++ packages/user-interface/src/components/Task.tsx
@@ -9,6 +9,7 @@ import useOgonePayment from "../hooks/useOgonePayment";
 import { Button } from "@gemeente-denhaag/components-react";
 import { useContext } from "react";
 import { LocaleContext } from "@nl-portal/nl-portal-localization";
+import { useHref, useLinkClickHandler } from "react-router-dom";
 
 interface Props {
   task: TaakV2;
@@ -20,65 +21,78 @@ const Task = ({ task, openInContext }: Props) => {
   const { currentLocale } = useContext(LocaleContext);
   const { startPayment, renderPaymentRedirectForm } = useOgonePayment();
   const taskUrl = useTaskUrl(task, openInContext);
-
-  if (task.soort === TaakSoort.Ogonebetaling) {
-    if (!task.ogonebetaling) {
-      return;
-    }
-
-    const paymentForm = renderPaymentRedirectForm();
-    if (paymentForm) {
-      return paymentForm;
-    }
-
-    let currentUrl = window.location.href;
-    const separator = currentUrl.indexOf("?") !== -1 ? "&" : "?";
-    currentUrl += `${separator}type=ogone`;
-
-    const paymentRequestPayload = {
-      amount: task.ogonebetaling?.bedrag,
-      orderId: task.id,
-      reference: "",
-      pspId: task.ogonebetaling.pspid,
-      title: task.titel,
-      langId: currentLocale.replace("-", "_"),
-      successUrl: `${currentUrl}&success=true`,
-      failureUrl: currentUrl,
-    };
-
-    return (
-      <Button onClick={() => startPayment(paymentRequestPayload)}>
-        Betalen
-      </Button>
-    );
+  const taskHref = useHref(taskUrl ?? "");
+  const handleClick = useLinkClickHandler(taskHref);
+  const paymentForm = renderPaymentRedirectForm();
+  if (paymentForm) {
+    return paymentForm;
   }
 
-  if (task.soort === TaakSoort.Url) {
+  if (openInContext && task.koppeling) {
     return (
-      <ActionMulti
+      <ActionSingle
         relativeDate
         labels={labels}
         dateTime={task.verloopdatum}
-        actions={
-          <ButtonLink href={taskUrl} target="_blank">
-            <ChevronRightIcon />
-          </ButtonLink>
-        }
+        link={taskUrl ?? ""}
+        Link={PortalLink}
       >
         {task.titel}
-      </ActionMulti>
+      </ActionSingle>
     );
   }
+
+  let actions = <></>;
+  switch (task.soort) {
+    case TaakSoort.Formtaak: {
+      actions = (
+        <ButtonLink href={taskHref} onClick={handleClick}>
+          Informatie geven
+        </ButtonLink>
+      );
+      break;
+    }
+    case TaakSoort.Url: {
+      actions = (
+        <ButtonLink href={taskUrl} target="_blank">
+          <ChevronRightIcon />
+        </ButtonLink>
+      );
+      break;
+    }
+    case TaakSoort.Ogonebetaling:
+      if (task.ogonebetaling) {
+        let currentUrl = window.location.href;
+        const separator = currentUrl.indexOf("?") !== -1 ? "&" : "?";
+        currentUrl += `${separator}type=ogone`;
+
+        const paymentRequestPayload = {
+          amount: task.ogonebetaling.bedrag,
+          orderId: task.id,
+          reference: "",
+          pspId: task.ogonebetaling.pspid,
+          title: task.titel,
+          langId: currentLocale.replace("-", "_"),
+          successUrl: `${currentUrl}&success=true`,
+          failureUrl: currentUrl,
+        };
+        actions = (
+          <Button onClick={() => startPayment(paymentRequestPayload)}>
+            Betalen
+          </Button>
+        );
+        break;
+      }
+  }
   return (
-    <ActionSingle
+    <ActionMulti
       relativeDate
       labels={labels}
       dateTime={task.verloopdatum}
-      link={taskUrl ?? ""}
-      Link={PortalLink}
+      actions={actions}
     >
       {task.titel}
-    </ActionSingle>
+    </ActionMulti>
   );
 };
 
