c0649949de4d809b9648e8d27936d9e8ea130b62
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-10-21 15:13:50 +0200

    fix: moved taakkoppelingregistratie enum to interfaces folder

--- packages/user-interface/src/hooks/useTaskUrl.tsx
+++ packages/user-interface/src/hooks/useTaskUrl.tsx
@@ -1,17 +1,14 @@
 import { useOutletContext } from "react-router-dom";
 import { RouterOutletContext } from "../interfaces/router-outlet-context";
-
 import { TaakSoort, TaakV2 } from "@nl-portal/nl-portal-api";
-
-enum TaakKoppeling {
-  Zaak = "ZAAK",
-  Product = "PRODUCT",
-}
+import { TaakKoppelingRegistratie } from "../interfaces/taak-koppeling-registratie";
 
 const useTaskUrl = (task: TaakV2, openInContext?: boolean) => {
   const { paths } = useOutletContext<RouterOutletContext>();
   if (openInContext && task.koppeling) {
-    if (task.koppeling.registratie.toUpperCase() === TaakKoppeling.Zaak)
+    if (
+      task.koppeling.registratie.toUpperCase() === TaakKoppelingRegistratie.Zaak
+    )
       return paths.case(task.koppeling.uuid);
   }
   if (task.soort === TaakSoort.Url) return task.url?.uri;
---
+++ packages/user-interface/src/interfaces/taak-koppeling-registratie.ts
@@ -0,0 +1,4 @@
+export enum TaakKoppelingRegistratie {
+  Zaak = "ZAAK",
+  Product = "PRODUCT",
+}
