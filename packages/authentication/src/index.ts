fb70c1c3f7509ca9b5d5225e541b0cea196d4aaf
Borai van Uden <info@borai.nl>
2024-10-21 19:23:46 +0200

    feat: create keycloak callback page

--- packages/authentication/src/index.ts
+++ packages/authentication/src/index.ts
@@ -1,3 +1,4 @@
 export { default as KeycloakWrapper } from "./components/KeycloakWrapper";
 export { default as KeycloakContext } from "./contexts/KeycloakContext";
+export { default as KeycloakCallbackPage } from "./pages/KeycloakCallbackPage";
 export * from "./utils/format-url-trailing-slash";
