5fbd9aeba5a3f189eac0de76bb931ab4974ca75c
Borai van Uden <info@borai.nl>
2024-10-22 20:01:32 +0200

    feat: ogone payment default settings

--- packages/user-interface/src/hooks/useOgonePayment.tsx
+++ packages/user-interface/src/hooks/useOgonePayment.tsx
@@ -3,20 +3,36 @@ import {
   OgonePayment,
   useGenerateOgonePaymentMutation,
 } from "@nl-portal/nl-portal-api";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import OgonePaymentForm from "../components/OgonePaymentForm";
+import { LocaleContext } from "@nl-portal/nl-portal-localization";
+import { Optional } from "../interfaces/optional";
 
 const useOgonePayment = () => {
   const [loading, setLoading] = useState(false);
   const [paymentData, setPaymentData] = useState<OgonePayment>();
   const [mutateFunction] = useGenerateOgonePaymentMutation();
+  const { currentLocale } = useContext(LocaleContext);
+  const successUrl = new URL(window.location.href);
+  successUrl.searchParams.set("type", "ogone");
+  successUrl.searchParams.set("success", "true");
+  const failureUrl = new URL(window.location.href);
+  failureUrl.searchParams.set("type", "ogone");
 
   const startPayment = (
-    paymentRequestPayload: GenerateOgonePaymentMutationVariables,
+    paymentRequestPayload: Optional<
+      GenerateOgonePaymentMutationVariables,
+      "langId" | "successUrl" | "failureUrl"
+    >,
   ) => {
     setLoading(true);
     mutateFunction({
-      variables: { ...paymentRequestPayload },
+      variables: {
+        langId: currentLocale.replace("-", "_"),
+        successUrl: successUrl.href,
+        failureUrl: failureUrl.href,
+        ...paymentRequestPayload,
+      },
       onCompleted: (data) => {
         setPaymentData(data.generateOgonePayment);
       },
