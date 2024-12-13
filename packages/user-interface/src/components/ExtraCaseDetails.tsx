75dc32861abdc7511b2f70f66b96cff03e24e4b1
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-12-13 17:11:44 +0100

    refactor: refactored extra zaak details for new schema

--- packages/user-interface/src/components/ExtraCaseDetails.tsx
+++ packages/user-interface/src/components/ExtraCaseDetails.tsx
@@ -1,197 +1,459 @@
 import { ReactNode } from "react";
 import { DescriptionList } from "@gemeente-denhaag/descriptionlist";
 import TableList from "./TableList";
-import UnorderedList, {
-  UnorderedListItem,
-} from "@gemeente-denhaag/unorderedlist";
-import { formatDate } from "@gemeente-denhaag/utils";
 import SectionHeader from "./SectionHeader";
-import { FormattedMessage } from "react-intl";
+import { useDateFormatter } from "@nl-portal/nl-portal-localization";
 
 interface Props {
   data: Details[];
 }
 
-export interface Extrazaakdetails {
+export interface ExtraZaakDetails {
   data: Details[];
-  zaak?: string;
+  zaak: string; // URI format
 }
 
 export interface Table {
-  headers: Details[];
-  rows: Details[][];
+  headers: Content[];
+  rows: Content[][];
 }
 
-export type Waarde = string | boolean | string[] | Details[] | Table;
+export enum ContentTypes {
+  BOLD = "bold",
+  ITALIC = "italic",
+  DATE = "date",
+}
 
-export type Types = "bold" | "italic" | "date" | "table";
+export enum DetailType {
+  TABLE = "table",
+  DESCRIPTION_LIST = "keywaardelijst",
+}
 
 export interface Details {
-  key?: string;
-  type?: Types;
-  waarde: Waarde;
-  heading?: string;
-  omschrijving?: string;
+  key?: string; // Optional key field, based on its appearance in "details"
+  type: DetailType.TABLE | DetailType.DESCRIPTION_LIST;
+  heading: string;
+  description?: string;
+  items: Content[] | Table;
+  children?: Details[]; // Recursive type for nested details
 }
 
-const hasDetailsWaardes = (waarde: Waarde) =>
-  Array.isArray(waarde) && waarde.every((item) => typeof item === "object");
-const hasStringWaardes = (waarde: Waarde) =>
-  Array.isArray(waarde) && waarde.every((item) => typeof item === "string");
+export interface Content {
+  key: string;
+  content: string;
+  type?: ContentTypes.BOLD | ContentTypes.ITALIC | ContentTypes.DATE;
+  description?: string;
+}
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const isTable = (obj: any): obj is Table => {
-  return (
-    typeof obj === "object" &&
-    obj !== null &&
-    typeof obj.headers === "object" &&
-    typeof obj.rows === "object"
-  );
-};
-
-const convertBoolean = (waarde: boolean) => {
-  return waarde
-    ? "extraCaseDetails.boolean.true"
-    : "extraCaseDetails.boolean.false";
-};
-
-const convertRichtText = (
-  waarde: Waarde,
-  type?: Types,
-): string | JSX.Element | JSX.Element[] | null => {
-  if (hasStringWaardes(waarde)) {
-    return waarde.map((item) => {
-      return (
-        <>
-          {convertRichtText(item, type)}
-          <br />
-        </>
-      );
-    });
-  }
-
-  if (typeof waarde !== "string" && typeof waarde !== "boolean") {
-    return null;
-  }
-
-  const value = typeof waarde === "boolean" ? convertBoolean(waarde) : waarde;
-
-  switch (type) {
-    case "bold":
-      return <b>{value}</b>;
-    case "italic":
-      return <i>{value}</i>;
-    case "date":
-      return formatDate({ dateTime: value })[0];
-    default:
-      return value;
-  }
-};
-
-const getTableSection = (details: Details, table: Table) => {
-  return (
-    <section>
-      <TableList
-        key={details.heading}
-        titleTranslationId={details.heading}
-        headers={table.headers?.map((head) => head.waarde.toString())}
-        rows={table.rows.map((row) =>
-          row.map((cell) => {
-            return {
-              children: convertRichtText(cell.waarde, cell.type),
-            };
-          }),
-        )}
-      />
-    </section>
-  );
-};
+const exampleData = {
+  zaak: "http://test.test",
+  data: [
+    {
+      heading: "Kosten",
+      type: "keywaardelijst",
+      items: [
+        {
+          key: "Locatie",
+          content: "Kamillestraat 27",
+          description: "Locatie waar de zaak betrekking op heeft",
+        },
+        {
+          key: "interne ref",
+          content: "OVX-4295272/A00/201723693",
+          description: "interne verwijzing naar Key2Vergunningen",
+        },
+      ],
+      children: [
+        {
+          heading: "Kosten nested",
+          type: "keywaardelijst",
+          description: "Een voorbeeld van nesting",
+          items: [
+            {
+              key: "Locatie",
+              content: "Kamillestraat 27",
+              description: "Locatie waar de zaak betrekking op heeft",
+            },
+            {
+              key: "interne ref",
+              content: "OVX-4295272/A00/201723693",
+              description: "interne verwijzing naar Key2Vergunningen",
+            },
+          ],
+        },
+        {
+          heading: "Betaalgeschiedenis",
+          type: "table",
+          items: {
+            rows: [
+              [
+                {
+                  key: "periode",
+                  type: "date",
+                  content: "2023-06-01",
+                },
+                {
+                  key: "bedrag",
+                  content: "€250",
+                },
+                {
+                  key: "status",
+                  content: "Openstaand",
+                },
+              ],
+              [
+                {
+                  key: "periode",
+                  type: "date",
+                  content: "2023-05-01",
+                },
+                {
+                  key: "bedrag",
+                  content: "€250",
+                },
+                {
+                  key: "status",
+                  content: "Openstaand",
+                },
+              ],
+              [
+                {
+                  key: "periode",
+                  type: "date",
+                  content: "2023-04-01",
+                },
+                {
+                  key: "bedrag",
+                  content: "€250",
+                },
+                {
+                  key: "status",
+                  content: "Openstaand",
+                },
+              ],
+              [
+                {
+                  key: "periode",
+                  type: "date",
+                  content: "2023-03-01",
+                },
+                {
+                  key: "bedrag",
+                  content: "€250",
+                },
+                {
+                  key: "status",
+                  content: "Betaald",
+                },
+              ],
+              [
+                {
+                  key: "periode",
+                  content: "",
+                  description:
+                    "Een voorbeeld van een lege row. Deze omschrijving is niet nodig voor een lege row.",
+                },
+                {
+                  key: "periode",
+                  content: "",
+                  description:
+                    "Een voorbeeld van een lege row. Deze omschrijving is niet nodig voor een lege row.",
+                },
+                {
+                  key: "periode",
+                  content: "",
+                  description:
+                    "Een voorbeeld van een lege row. Deze omschrijving is niet nodig voor een lege row.",
+                },
+              ],
+              [
+                {
+                  key: "periode",
+                  type: "bold",
+                  content: "Totaal",
+                },
+                {
+                  key: "bedrag",
+                  content: "€1000",
+                },
+                {
+                  key: "status",
+                  content: "Betaald",
+                },
+              ],
+            ],
+            headers: [
+              {
+                key: "periode",
+                content: "Periode",
+                description: "Periode waarover betaald is",
+              },
+              {
+                key: "bedrag",
+                content: "Bedrag",
+              },
+              {
+                key: "status",
+                content: "Status",
+                description: "Is het betaald?",
+              },
+            ],
+          },
+        },
+      ],
+    },
+    {
+      heading: "Betaalgeschiedenis 2",
+      type: "table",
+      items: {
+        rows: [
+          [
+            {
+              key: "periode",
+              type: "date",
+              content: "2023-06-01",
+            },
+            {
+              key: "bedrag",
+              content: "€250",
+            },
+            {
+              key: "status",
+              content: "Openstaand",
+            },
+          ],
+          [
+            {
+              key: "periode",
+              type: "date",
+              content: "2023-05-01",
+            },
+            {
+              key: "bedrag",
+              content: "€250",
+            },
+            {
+              key: "status",
+              content: "Openstaand",
+            },
+          ],
+          [
+            {
+              key: "periode",
+              type: "date",
+              content: "2023-04-01",
+            },
+            {
+              key: "bedrag",
+              content: "€250",
+            },
+            {
+              key: "status",
+              content: "Openstaand",
+            },
+          ],
+          [
+            {
+              key: "periode",
+              type: "date",
+              content: "2023-03-01",
+            },
+            {
+              key: "bedrag",
+              content: "€250",
+            },
+            {
+              key: "status",
+              content: "true",
+            },
+          ],
+          [
+            {
+              key: "periode",
+              content: "",
+              description:
+                "Een voorbeeld van een lege row. Deze omschrijving is niet nodig voor een lege row.",
+            },
+          ],
+          [
+            {
+              key: "periode",
+              type: "bold",
+              content: "Totaal",
+            },
+            {
+              key: "bedrag",
+              content: "€1000",
+            },
+            {
+              key: "status",
+              content: "Betaald",
+            },
+          ],
+        ],
+        headers: [
+          {
+            key: "periode",
+            content: "Periode",
+            description: "Periode waarover betaald is",
+          },
+          {
+            key: "bedrag",
+            content: "Bedrag",
+          },
+          {
+            key: "status",
+            content: "Status",
+            description: "Is het betaald?",
+          },
+        ],
+      },
+      children: [
+        {
+          heading: "Kosten nested 2",
+          type: "keywaardelijst",
+          items: [
+            {
+              key: "Locatie",
+              content: "Kamillestraat 27",
+              description: "Locatie waar de zaak betrekking op heeft",
+            },
+            {
+              key: "interne ref",
+              content: "OVX-4295272/A00/201723693",
+              description: "interne verwijzing naar Key2Vergunningen",
+            },
+          ],
+          children: [
+            {
+              heading: "Kosten nested 3",
+              type: "keywaardelijst",
+              items: [
+                {
+                  key: "Locatie",
+                  content: "Kamillestraat 27",
+                  description: "Locatie waar de zaak betrekking op heeft",
+                },
+                {
+                  key: "interne ref",
+                  content: "OVX-4295272/A00/201723693",
+                  description: "interne verwijzing naar Key2Vergunningen",
+                },
+              ],
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: "Special waardes",
+      type: "keywaardelijst",
+      items: [
+        {
+          key: "Title",
+          content: "This is a bold waarde",
+          type: "bold",
+          description: "A omschrijving for the bold title",
+        },
+        {
+          key: "Title",
+          content: "This is an italic waarde",
+          type: "italic",
+          description: "A omschrijving for the italic text",
+        },
+        {
+          key: "Date",
+          content: "2024-09-11",
+          type: "date",
+          description: "The date of the event",
+        },
+        {
+          key: "Boolean",
+          content: "true",
+          description: "boolean true waarde",
+        },
+        {
+          key: "Boolean 2",
+          content: "true",
+          description: "boolean false waarde",
+        },
+      ],
+    },
+  ],
+} as ExtraZaakDetails;
 
 export const ExtraCaseDetails = ({ data }: Props) => {
-  if (!data) return null;
+  const { formatDate } = useDateFormatter();
+  console.log(data);
+  //if (!data) return null;
   const zaakDetailsContent: ReactNode[] = [];
-  data.forEach((detail) => {
-    const headingElement = detail.heading ? (
-      <SectionHeader title={detail.heading} />
-    ) : undefined;
-    const waarde = detail.waarde;
 
-    // Waarde type string[]
-    if (hasStringWaardes(waarde)) {
+  const convertRichtText = (
+    value: string,
+    type?: ContentTypes,
+  ): string | JSX.Element | JSX.Element[] | null => {
+    switch (type) {
+      case ContentTypes.BOLD:
+        return <b>{value}</b>;
+      case ContentTypes.ITALIC:
+        return <i>{value}</i>;
+      case ContentTypes.DATE:
+        return formatDate({ date: value });
+      default:
+        return value;
+    }
+  };
+
+  const getTableSection = (heading: string, table: Table) => {
+    return (
+      <section key={`section-${heading}`}>
+        <TableList
+          key={heading}
+          titleTranslationId={heading}
+          headers={table.headers.map((head) => head.content)}
+          rows={table.rows.map((row) =>
+            row.map((cell) => {
+              return {
+                children: convertRichtText(cell.content, cell.type),
+              };
+            }),
+          )}
+        />
+      </section>
+    );
+  };
+
+  const getContentElements = (detail: Details, isChild: boolean) => {
+    if (detail.type === DetailType.TABLE) {
       zaakDetailsContent.push(
-        <section>
-          {headingElement}
-          <UnorderedList>
-            {waarde.map((item, index) => (
-              <UnorderedListItem key={index}>
-                {convertRichtText(item, detail.type)}
-              </UnorderedListItem>
-            ))}
-          </UnorderedList>
-        </section>,
+        getTableSection(detail.heading, detail.items as Table),
       );
     }
 
-    // Waarde type Details
-    if (hasDetailsWaardes(waarde)) {
-      const descriptionListItems = waarde.filter(
-        (item) => item.type !== "table",
-      );
-      const tableItems = waarde.filter((item) => item.type === "table");
-
-      const dlItems = descriptionListItems
-        .filter(
-          (item) =>
-            typeof item.waarde === "string" || typeof item.waarde === "boolean",
-        )
-        .map((item) => {
-          return {
-            title: item.key,
-            detail: convertRichtText(item.waarde, item.type),
-          };
-        });
-
+    if (detail.type === DetailType.DESCRIPTION_LIST) {
       zaakDetailsContent.push(
-        <section>
-          {headingElement}
-          <DescriptionList items={dlItems} />
-        </section>,
-      );
-
-      tableItems
-        .filter((item) => isTable(item.waarde))
-        .forEach((tableDetail) => {
-          const table = tableDetail.waarde as Table;
-          zaakDetailsContent.push(getTableSection(tableDetail, table));
-        });
-    }
-
-    // Waarde type Table
-    if (isTable(waarde)) {
-      zaakDetailsContent.push(getTableSection(detail, waarde));
-    }
-
-    // Waarde types string | boolean
-    if (typeof waarde === "string" || typeof waarde === "boolean") {
-      zaakDetailsContent.push(
-        <section>
-          {headingElement}
+        <section key={`section-${detail.heading}`}>
+          <SectionHeader title={detail.heading} small={isChild} />
           <DescriptionList
-            items={[
-              {
-                title: detail.key,
-                detail:
-                  typeof waarde === "string" ? (
-                    convertRichtText(waarde, detail.type)
-                  ) : (
-                    <FormattedMessage id={convertBoolean(waarde)} />
-                  ),
-              },
-            ]}
+            items={(detail.items as Content[]).map((item) => {
+              return {
+                title: item.key,
+                detail: convertRichtText(item.content, item.type),
+              };
+            })}
           />
         </section>,
       );
+
+      if (!isChild) {
+        detail.children?.forEach((child) => getContentElements(child, true));
+      }
     }
+  };
+
+  exampleData.data.forEach((detail) => {
+    getContentElements(detail, false);
   });
 
   return zaakDetailsContent;
