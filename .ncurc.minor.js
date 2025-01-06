82018c9ef6627a8996160ff1876f797eab3b6cac
Borai van Uden <info@borai.nl>
2025-01-06 16:00:33 +0100

    fix: dev with formiojs down grade

--- .ncurc.minor.js
+++ .ncurc.minor.js
@@ -1,9 +1,12 @@
 const patchConfig = require("./.ncurc.patch");
 
-// Support for React 19 is removed in minor release 7.1.0. New MR is available: https://github.com/formatjs/formatjs/pull/4797
+/**
+ * React-intl: Support for React 19 is removed in minor release 7.1.0. New MR is available: https://github.com/formatjs/formatjs/pull/4797
+ * formiojs: App breaks when upgrading minor
+ **/
 
 module.exports = {
   ...patchConfig,
-  reject: [...patchConfig.reject, "react-intl"],
+  reject: [...patchConfig.reject, "react-intl", "formiojs"],
   target: "minor",
 };
