8fbb3a077f428f426b62b2acd39fe5eaee7357db
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2025-02-21 15:38:37 +0100

    feat: added option to show warning before automatic logging out

--- packages/authentication/src/components/ProtectedApp.tsx
+++ packages/authentication/src/components/ProtectedApp.tsx
@@ -1,20 +1,27 @@
-import { PropsWithChildren, useContext, useEffect } from "react";
+import { PropsWithChildren, useContext, useEffect, useState } from "react";
 import { useAuth } from "react-oidc-context";
 import KeycloakContext from "../contexts/KeycloakContext";
 import IdleTimer from "./IdleTimer";
+import Modal from "@gemeente-denhaag/modal";
+import { Paragraph } from "@gemeente-denhaag/typography";
+import { FormattedMessage, useIntl } from "react-intl";
 
 export type ProtectedAppProps = {
   autoIdleSessionLogout?: boolean;
   idleTimeoutMinutes?: number;
+  showAutomaticLogoutWarning?: boolean;
 };
 
 export const ProtectedApp = ({
   autoIdleSessionLogout = true,
-  idleTimeoutMinutes = 5,
+  idleTimeoutMinutes = 15,
+  showAutomaticLogoutWarning = true,
   children,
 }: PropsWithChildren<ProtectedAppProps>) => {
   const auth = useAuth();
   const { keycloakToken, setKeycloakToken } = useContext(KeycloakContext);
+  const [showLogoutWarning, setShowLogoutWarning] = useState(false);
+  const intl = useIntl();
 
   useEffect(() => {
     // Redirect to keycloak when not loading and user isn't authenticated or if there is no keycloak token in the state (after hard refresh for example)
@@ -39,10 +46,50 @@ export const ProtectedApp = ({
     return (
       <>
         {autoIdleSessionLogout && (
-          <IdleTimer
-            idleTimeoutMinutes={idleTimeoutMinutes}
-            onTimeOut={auth.signoutRedirect}
-          />
+          <>
+            <IdleTimer
+              idleTimeoutMinutes={idleTimeoutMinutes}
+              onTimeOut={auth.signoutRedirect}
+              onWarning={
+                showAutomaticLogoutWarning
+                  ? () => setShowLogoutWarning(true)
+                  : undefined
+              }
+            />
+            {showLogoutWarning && (
+              <Modal
+                open={showLogoutWarning}
+                title={intl.formatMessage({ id: "auth.inactive.title" })}
+                closeLabel={intl.formatMessage({ id: "auth.inactive.close" })}
+                onToggle={(open) => setShowLogoutWarning(open)}
+                actions={(toggle) => [
+                  {
+                    children: intl.formatMessage({
+                      id: "auth.inactive.logout",
+                    }),
+                    variant: "secondary-action",
+                    onClick: () => {
+                      auth.signoutRedirect();
+                    },
+                  },
+                  {
+                    children: intl.formatMessage({
+                      id: "auth.inactive.stayLoggedIn",
+                    }),
+                    variant: "primary-action",
+                    onClick: () => {
+                      setShowLogoutWarning(false);
+                      toggle();
+                    },
+                  },
+                ]}
+              >
+                <Paragraph>
+                  <FormattedMessage id="auth.inactive.text" />
+                </Paragraph>
+              </Modal>
+            )}
+          </>
         )}
         {children}
       </>
