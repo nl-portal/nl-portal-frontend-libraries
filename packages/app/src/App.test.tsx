8b147991c83d09d4f64b70a47e145ea564ffdad4
Borai van Uden <info@borai.nl>
2025-02-27 12:09:26 +0100

    fix: removed unused test

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,15 +1,5 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
-import App from "./App";
+import { describe, it } from "vitest";
 
 describe("App", () => {
-  const openZaak1 = () => screen.getByText("case.OPENZAAK1.title");
-
-  it("renders without crashing", async () => {
-    render(<App />);
-
-    await waitFor(() => {
-      expect(openZaak1()).toBeVisible();
-    });
-  });
+  it("is truthy", () => {});
 });
