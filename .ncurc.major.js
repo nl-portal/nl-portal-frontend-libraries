d532194c0e06df3e7998440542ce92755d0b23e5
Borai van Uden <info@borai.nl>
2025-02-27 11:18:34 +0100

    feat: upgrade packages major

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -2,6 +2,18 @@ const minorConfig = require("./.ncurc.minor");
 
 module.exports = {
   ...minorConfig,
-  reject: [...minorConfig.reject],
+  reject: [
+    ...minorConfig.reject,
+    "keycloak-js",
+    "@react-keycloak/web",
+    "react",
+    "react-dom",
+    "react-router-dom",
+    "@types/react",
+    "@types/react-dom",
+    "@types/react-router-dom",
+    "bootstrap",
+    "@formio/react",
+  ],
   target: "latest",
 };
