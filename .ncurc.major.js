a0aba5d141cd5fa0432f3eb5e81556bca9dbcf55
Borai van Uden <info@borai.nl>
2025-04-02 14:23:48 +0200

    feat: patch again

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -2,16 +2,6 @@ const minorConfig = require("./.ncurc.minor");
 
 module.exports = {
   ...minorConfig,
-  reject: [
-    ...minorConfig.reject,
-    "keycloak-js",
-    "@react-keycloak/web",
-    "react",
-    "react-dom",
-    "@types/react",
-    "@types/react-dom",
-    "bootstrap",
-    "@formio/react",
-  ],
+  reject: [...minorConfig.reject, "bootstrap", "@formio/react"],
   target: "latest",
 };
