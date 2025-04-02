61a4afb2fb0a95c3c81e308c1f3fd7a8281fcbd4
Borai van Uden <info@borai.nl>
2025-04-02 17:09:25 +0200

    chore: note why not upgrade major

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -1,5 +1,8 @@
 const minorConfig = require("./.ncurc.minor");
 
+// bootstrap: https://help.form.io/developers/css-frameworks#bootstrap-5
+// @formio/react: https://github.com/formio/react/issues/603
+
 module.exports = {
   ...minorConfig,
   reject: [...minorConfig.reject, "bootstrap", "@formio/react"],
