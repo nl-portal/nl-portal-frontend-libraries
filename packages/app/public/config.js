623e81a71ef6cebee2878be2ca862ab2fa230792
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2025-05-06 13:23:34 +0200

    fix: added backwards compatibility to cases search

--- packages/app/public/config.js
+++ packages/app/public/config.js
@@ -6,4 +6,5 @@ window.GRAPHQL_URI = 'http://localhost:8090/graphql';
 window.REST_URI = 'http://localhost:8090/api';
 window.SHOW_INHABITANT_AMOUNT = 'true';
 window.MESSAGE_COUNT_POLLING_INTERVAL = 10000;
+window.CASES_PARTIAL_SEARCH = true;
 window.ADDRESS_RESEARCH_URL = 'https://www.google.nl';
