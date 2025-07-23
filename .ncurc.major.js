983d1f3a1b19be91a5209829b5d77f9c4754c0b6
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2025-07-23 13:17:31 +0200

    chore: removed comment

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -1,8 +1,5 @@
 const minorConfig = require("./.ncurc.minor");
 
-// bootstrap: https://help.form.io/developers/css-frameworks#bootstrap-5
-// @formio/react: https://github.com/formio/react/issues/603
-
 module.exports = {
   ...minorConfig,
   reject: [...minorConfig.reject, "bootstrap"],
