db967e8603e733b6387bd97646efaf2c25f93e01
Borai van Uden <info@borai.nl>
2025-07-30 14:39:52 +0200

    fix: graph product taken pagesize

--- packages/api/src/queries/get-open-product-thema-zaken.ts
+++ packages/api/src/queries/get-open-product-thema-zaken.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_GET_OPEN_PRODUCT_THEMA_ZAKEN = gql`
-  query getOpenProductThemaZaken($id: UUID!, $pageSize: Int) {
-    getOpenProductThemaZaken(id: $id, pageSize: $pageSize) {
+  query getOpenProductThemaZaken($id: UUID!) {
+    getOpenProductThemaZaken(id: $id) {
       uuid
       omschrijving
       identificatie
