7402860e9539a9c05b99d8e9a998309037cda6d2
Borai van Uden <info@borai.nl>
2025-09-04 15:02:14 +0200

    fix: use all props

--- packages/user-interface/src/components/Pre.tsx
+++ packages/user-interface/src/components/Pre.tsx
@@ -2,8 +2,8 @@ import styles from "./Pre.module.scss";
 
 type PreProps = React.HTMLAttributes<HTMLPreElement>;
 
-const Pre = ({ children }: PreProps) => {
-  return <pre className={styles.pre}>{children}</pre>;
+const Pre = (props: PreProps) => {
+  return <pre className={styles.pre} {...props} />;
 };
 
 export default Pre;
