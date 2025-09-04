6a72c7810ad89895d8bdcb01d57eb696525de89b
Borai van Uden <info@borai.nl>
2025-09-04 15:03:30 +0200

    fix: styling

--- packages/user-interface/src/components/Pre.tsx
+++ packages/user-interface/src/components/Pre.tsx
@@ -1,9 +1,10 @@
+import classNames from "classnames";
 import styles from "./Pre.module.scss";
 
 type PreProps = React.HTMLAttributes<HTMLPreElement>;
 
 const Pre = (props: PreProps) => {
-  return <pre className={styles.pre} {...props} />;
+  return <pre className={classNames(styles.pre, props.className)} {...props} />;
 };
 
 export default Pre;
