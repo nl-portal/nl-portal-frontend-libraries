86bdfcf2b916982c03c1ef35d04e2a4842a39008
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2025-12-02 17:43:32 +0100

    fix: temporary remove attempt to make upload component better

--- packages/user-interface/src/components/formio/FormIoUploader.tsx
+++ packages/user-interface/src/components/formio/FormIoUploader.tsx
@@ -5,11 +5,9 @@ import { FormattedMessage } from "react-intl";
 import { get } from "lodash-es";
 import { FormField } from "@gemeente-denhaag/form-field";
 import { FormLabel } from "@gemeente-denhaag/form-label";
+import { TextInput } from "@gemeente-denhaag/text-input";
 import { FormFieldErrorMessage } from "@gemeente-denhaag/form-field-error-message";
 import { LocalizationProvider } from "@nl-portal/nl-portal-localization";
-import "./FormIoUploader.scss";
-import { Button } from "@gemeente-denhaag/button";
-import { File } from "@gemeente-denhaag/file";
 
 export interface UploadedFile {
   url: string;
@@ -130,13 +128,7 @@ const FileUpload = ({
   return (
     <FormField invalid={error}>
       <FormLabel htmlFor={id}>{label}</FormLabel>
-      <Button
-        variant="secondary-action"
-        onClick={() => fileInputRef.current?.click()}
-      >
-        Bestand kiezen
-      </Button>
-      <input
+      <TextInput
         ref={fileInputRef}
         className="nl-portal-file-upload"
         id={id}
@@ -144,6 +136,7 @@ const FileUpload = ({
         name="file"
         onChange={onChangeHandler}
         disabled={disabled || isLoading}
+        invalid={error}
         {...attributes}
       />
       {!isLoading && error && (
@@ -153,15 +146,10 @@ const FileUpload = ({
       )}
       {!isLoading &&
         fileList.map((file) => (
-          <File
-            name={file.name}
-            size={String(file.size)}
-            key={file.url}
-            href=""
-            onClick={() =>
-              setFileList((prev) => prev.filter((f) => file.url !== f.url))
-            }
-          />
+          <div key={file.url}>
+            <p>Filename: {file.name}</p>
+            <p>Filesize: {file.size}</p>
+          </div>
         ))}
       {isLoading && <p>Loading</p>}
     </FormField>
