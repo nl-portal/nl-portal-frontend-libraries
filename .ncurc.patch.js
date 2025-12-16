4790c83e10cf04283ca3fcfb0d3e866d2d1c344b
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2025-12-16 17:03:30 +0100

    chore: dependency updates without apollo and linting

--- .ncurc.patch.js
+++ .ncurc.patch.js
@@ -2,7 +2,15 @@ module.exports = {
   cooldown: 1, // 1 day
   dep: ["dev", "prod", "peer"],
   install: "always",
-  reject: [],
+  reject: [
+    "@apollo/client",
+    "graphql",
+    "@graphql-codegen/cli",
+    "@graphql-codegen/typescript-operations",
+    "@graphql-codegen/typescript-react-apollo",
+    "eslint",
+    "eslint-plugin-react-hooks",
+  ],
   root: true,
   target: "patch",
   upgrade: true,
