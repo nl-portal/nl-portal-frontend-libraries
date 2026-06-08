9bce5f576995d8b42034817b6a30bb160924ad77
Anna Vissart <anna.vissart@ritense.com>
2026-06-08 12:13:30 +0200

    feat: re-add case queries and types

--- packages/api/src/generated/Graphql.tsx
+++ packages/api/src/generated/Graphql.tsx
@@ -594,6 +594,35 @@ export type BrpVerblijfsTitelInOnderzoek = {
   datumIngangOnderzoek?: Maybe<BrpDatum>;
 };
 
+export type CaseCreated = {
+  __typename?: 'CaseCreated';
+  caseId?: Maybe<Scalars['UUID']['output']>;
+};
+
+export type CaseDefinition = {
+  __typename?: 'CaseDefinition';
+  id: Scalars['String']['output'];
+  schema: Scalars['JSON']['output'];
+  statusDefinition: Array<Maybe<Scalars['String']['output']>>;
+};
+
+export type CaseInstance = {
+  __typename?: 'CaseInstance';
+  caseDefinitionId: Scalars['String']['output'];
+  createdOn: Scalars['String']['output'];
+  externalId?: Maybe<Scalars['String']['output']>;
+  id?: Maybe<Scalars['UUID']['output']>;
+  status?: Maybe<Status>;
+  statusHistory?: Maybe<Array<Maybe<HistoricStatus>>>;
+  submission?: Maybe<Scalars['JSON']['output']>;
+  userId: Scalars['String']['output'];
+};
+
+export enum CaseInstanceOrdering {
+  Asc = 'ASC',
+  Desc = 'DESC'
+}
+
 export type Categorie = {
   __typename?: 'Categorie';
   naam: Scalars['String']['output'];
@@ -784,6 +813,12 @@ export type HandelsNaam = {
   volgorde: Scalars['Int']['output'];
 };
 
+export type HistoricStatus = {
+  __typename?: 'HistoricStatus';
+  createdOn: Scalars['String']['output'];
+  status: Status;
+};
+
 export type Hoofdvestiging = {
   __typename?: 'Hoofdvestiging';
   adressen?: Maybe<Array<Maybe<Adres>>>;
@@ -858,6 +893,7 @@ export type Mutation = {
   doDirectPayment: DirectPaymentResponse;
   /**  Create Ogone payment with hash and fields */
   generateOgonePayment: OgonePayment;
+  processSubmission?: Maybe<CaseCreated>;
   /**  Submit a task */
   submitTaakV2: TaakV2;
   /**  Update product */
@@ -893,6 +929,13 @@ export type MutationDoDirectPaymentArgs = {
 
 export type MutationGenerateOgonePaymentArgs = {
   paymentRequest: OgonePaymentRequestInput;
+};
+
+
+export type MutationProcessSubmissionArgs = {
+  caseDefinitionId: Scalars['String']['input'];
+  initialStatus?: InputMaybe<Scalars['String']['input']>;
+  submission?: InputMaybe<Scalars['JSON']['input']>;
 };
 
 
@@ -1900,6 +1943,8 @@ export type ProductenPage = {
 
 export type Query = {
   __typename?: 'Query';
+  allCaseDefinitions: Array<Maybe<CaseDefinition>>;
+  allCaseInstances?: Maybe<Array<Maybe<CaseInstance>>>;
   /**  Find the Partij of the authenticated user. */
   findUserPartij?: Maybe<OpenKlant2Partij>;
   /**  Gets the bedrijf data */
@@ -1921,6 +1966,7 @@ export type Query = {
   getBesluiten: BesluitPage;
   /**  Gets the number of people living in the same house of the adresseerbaarObjectIdentificatie */
   getBewonersAantalV2?: Maybe<Scalars['Int']['output']>;
+  getCaseInstance?: Maybe<CaseInstance>;
   getDecision: Array<Scalars['JSON']['output']>;
   getDirectPaymentStatus: DirectPaymentStatus;
   /**
@@ -2026,6 +2072,11 @@ export type Query = {
 };
 
 
+export type QueryAllCaseInstancesArgs = {
+  orderBy: CaseInstanceOrdering;
+};
+
+
 export type QueryGetBerichtArgs = {
   id: Scalars['UUID']['input'];
 };
@@ -2076,6 +2127,11 @@ export type QueryGetBesluitenArgs = {
 
 export type QueryGetBewonersAantalV2Args = {
   adresseerbaarObjectIdentificatie: Scalars['String']['input'];
+};
+
+
+export type QueryGetCaseInstanceArgs = {
+  id?: InputMaybe<Scalars['UUID']['input']>;
 };
 
 
@@ -2389,6 +2445,17 @@ export enum SoortPartij {
   Organisatie = 'ORGANISATIE',
   Persoon = 'PERSOON'
 }
+
+export enum Sort {
+  Asc = 'ASC',
+  Desc = 'DESC'
+}
+
+export type Status = {
+  __typename?: 'Status';
+  createdOn: Scalars['String']['output'];
+  name: Scalars['String']['output'];
+};
 
 export type StatusType = {
   __typename?: 'StatusType';
